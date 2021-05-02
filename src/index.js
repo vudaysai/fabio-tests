f24ff669854a82d1153adc5733010567748cd642
Uday <udayvsai@gmail.com>
2021-05-03 01:32:49 +0530

    Cleanup

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import en from 'javascript-time-ago/locale/en';
 import './index.css';
 import App from './App';
 import worker from './mocks';
-import reportWebVitals from './reportWebVitals';
 
 TimeAgo.addDefaultLocale(en);
 worker.start();
@@ -20,8 +19,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
