6ff96683fb73813be4bffaa23c70df8fccf2838b
Uday <udayvsai@gmail.com>
2021-05-02 15:17:52 +0530

    Initialized with CRA and Integrated TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen bg-gray-200 ">
+      <div className="p-6 max-w-sm m-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        Built With React and TailwindCSS
+      </div>
     </div>
   );
 }
